31e5e8554ad2835255546cd2c0d8760cb5ac3e58
Eugene2208 <47624046+Eugene2208@users.noreply.github.com>
2020-05-22 13:39:29 +0300

    Add files via upload

--- hw12/src/main/java/hw10/Calculator.java
+++ hw12/src/main/java/hw10/Calculator.java
@@ -6,23 +6,23 @@ import org.apache.logging.log4j.Logger;
 
 public class Calculator {
 
-    private static final Logger log = LogManager.getLogger(Calculator.class);
+    private static final Logger LOG = LogManager.getLogger(Calculator.class);
 
     public static int sum(int first, int second){
         int result = first + second;
-        log.debug(String.format("SUM: first: %d, second: %d, result: %d", first, second, result));
+        LOG.debug(String.format("SUM: first: %d, second: %d, result: %d", first, second, result));
         return result;
     }
 
     public static int minus(int first, int second){
         int result = first - second;
-        log.info(String.format("MINUS: first: %d, second: %d, result: %d", first, second, result));
+        LOG.info(String.format("MINUS: first: %d, second: %d, result: %d", first, second, result));
         return result;
     }
 
     public static int multiply(int first, int second){
         int result = first * second;
-        log.warn(String.format("MULTIPLY: first: %d, second: %d, result: %d", first, second, result));
+        LOG.warn(String.format("MULTIPLY: first: %d, second: %d, result: %d", first, second, result));
         return result;
     }
 
@@ -32,11 +32,11 @@ public class Calculator {
         try {
             result = first / second;
         }catch (ArithmeticException e){
-            log.fatal("DIVISION BY ZERO");
+            LOG.fatal("DIVISION BY ZERO");
             throw e;
         }
 
-        log.error(String.format("DIVIDE: first: %d, second: %d, result: %d", first, second, result));
+        LOG.error(String.format("DIVIDE: first: %d, second: %d, result: %d", first, second, result));
         return result;
     }
 }
